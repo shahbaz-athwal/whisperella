9b07612e9f1f63616ae80fd433499fb73c09e93d
Shahbaz <shahbazathwal2107@gmail.com>
2024-06-26 11:49:35 +0530

    signup tested and unique username check added

---
+++ src/app/api/uniqueusername/route.ts
@@ -0,0 +1,61 @@
+import db from "@/lib/db";
+import z from "zod"
+import { usernameSchema, signUpSchema } from "@/schema/signUp";
+
+const UsernameQuerySchema = z.object({
+    username: usernameSchema
+})
+
+export async function GET(request: Request) {
+    try {
+        const {searchParams} = new URL(request.url)
+        const queryParam= {
+            username: searchParams.get('username')
+        }
+        const result = UsernameQuerySchema.safeParse(queryParam)
+        console.log(result)
+
+        if (!result.success) {
+            const errors = result.error.format().username?._errors || []
+            return Response.json({
+                success: false,
+                message: "Username format in incorrect."
+            },
+            {
+                status: 400
+            })
+
+        }
+        const { username } = result.data
+        const userFromDb = await db.user.findUnique({
+            where: {
+                username: username
+            }
+        })
+        if (userFromDb) {
+            return Response.json({
+                success: false,
+                message: "Username is taken."
+            },
+            {
+                status: 409
+            })
+        }
+        return Response.json({
+            success: true,
+            message: "Username is available."
+        },
+        {
+            status: 409
+        })
+    } catch (error) {
+        console.error("Error checking username.",error)
+        return Response.json({
+            success: false,
+            message: "Error checking username."
+        },
+        {
+            status: 400
+        })
+    }
+}
--- src/schema/signUp.ts
+++ src/schema/signUp.ts
@@ -1,11 +1,13 @@
 import { z } from "zod";
 
+export const usernameSchema = z
+  .string()
+  .min(3)
+  .max(30)
+  .regex(/^[a-zA-Z0-9._]+$/, "Invalid username");
+
 export const signUpSchema = z.object({
-  username: z
-    .string()
-    .min(3)
-    .max(30)
-    .regex(/^[a-zA-Z0-9._]+$/, "Invalid username"),
+  username: usernameSchema,
 
   email: z.string().email({ message: "Invalid email" }),
   password: z
