11835b5f6d42a1fe260b79be5e4a6e3b7e9ba7c0
Shahbaz <shahbazathwal2107@gmail.com>
2024-07-21 14:51:44 +0530

    review fix

--- src/app/api/getreviews/route.ts
+++ src/app/api/getreviews/route.ts
@@ -1,6 +1,21 @@
 import db from "@/lib/db";
 
 export async function GET(request: Request) {
+    
     const reviews = await db.review.findMany()
-    return Response.json({reviews: reviews.reverse()})
+    if (!reviews) {
+        return Response.json({
+            success: false,
+            message: "Could not get reviews"
+        },{
+            status: 411
+        })
+    }
+    return Response.json({
+        success: true,
+        reviews: reviews.reverse()
+        
+    },{
+        status: 200
+    })
 }
