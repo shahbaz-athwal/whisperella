b5559d36dfa0f8e1df40ad61cc92379578740181
Shahbaz <shahbazathwal2107@gmail.com>
2024-07-21 14:43:34 +0530

    render fix

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -34,7 +34,7 @@ export default function Page() {
     } catch (error) {
       console.error('Error fetching reviews:', error);
     }
-  }, []);
+  }, [setReviews]);
 
   useEffect(() => {
     getReviews();
