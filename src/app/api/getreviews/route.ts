6e88096ff3fed3fd68dc6cea1768d8c3346f71e3
Shahbaz <shahbazathwal2107@gmail.com>
2024-07-21 15:43:28 +0530

    session added

--- src/app/api/getreviews/route.ts
+++ src/app/api/getreviews/route.ts
@@ -1,6 +1,17 @@
+import { auth } from "@/auth";
 import db from "@/lib/db";
 
 export async function GET(request: Request) {
+    const session = await auth()
+    if (!session || !session.user) {
+        return Response.json({
+            success: false,
+            message: "Not authenticated"
+        },{
+            status: 401
+        })
+    }
     const reviews = await db.review.findMany();
+    console.log(reviews)
     return Response.json({ reviews: reviews.reverse() });
 }
