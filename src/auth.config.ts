631cf787c497c0503c5fe6e887c7f6ee85ae5147
Shahbaz <shahbazathwal2107@gmail.com>
2024-08-15 17:27:37 +0530

    account linking

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,59 +1,60 @@
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
-import Credentials from "next-auth/providers/credentials"
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import db from "@/lib/db";
 
-import type { NextAuthConfig } from "next-auth"
- 
-export default { 
-    providers: [
-          GitHub({
-            clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!,
-            
-          }),
-          Google({
-            clientId: process.env.GOOGLE_ID!,
-            clientSecret: process.env.GOOGLE_SECRET!,
-          }),
-          Credentials({
-            async authorize(credentials: any): Promise<any> {
-              try {
-                const user = await db.user.findFirst({
-                  where: {
-                    OR: [
-                      {
-                        email: credentials.identifier,
-                      },
-                      {
-                        username: credentials.identifier,
-                      },
-                    ],
-                  },
-                });
-                if (!user) {
-                  throw new Error("User not found.");
-                }
-                if (!user.password) {
-                  throw new Error("Signin with Google or Github");
-                }
-                if (!user.isVerified) {
-                  throw new Error("Please verify your email.");
-                }
-                const isPasswordCorrect = await bcrypt.compare(
-                  credentials.password,
-                  user.password!
-                );
-                if (isPasswordCorrect) {
-                  return user;
-                } else {
-                  throw new Error("Incorrect Password");
-                }
-              } catch (error: any) {
-                throw new Error(error);
-              }
-            }
-          }),
-        ]
- } satisfies NextAuthConfig
+import type { NextAuthConfig } from "next-auth";
+
+export default {
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+      allowDangerousEmailAccountLinking: true,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+      allowDangerousEmailAccountLinking: true,
+    }),
+    Credentials({
+      async authorize(credentials: any): Promise<any> {
+        try {
+          const user = await db.user.findFirst({
+            where: {
+              OR: [
+                {
+                  email: credentials.identifier,
+                },
+                {
+                  username: credentials.identifier,
+                },
+              ],
+            },
+          });
+          if (!user) {
+            throw new Error("User not found.");
+          }
+          if (!user.password) {
+            throw new Error("Signin with Google or Github");
+          }
+          if (!user.isVerified) {
+            throw new Error("Please verify your email.");
+          }
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password!
+          );
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Incorrect Password");
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
